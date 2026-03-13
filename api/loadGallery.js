2b0df23f4937665abb77f672ddc0e8821327f119
Arealkeetu <airilsyazani25@gmail.com>
2026-03-13 21:06:03 +0800

    Update loadGallery.js

--- api/loadGallery.js
+++ api/loadGallery.js
@@ -1,16 +1,32 @@
-import fs from 'fs';
-import path from 'path';
+export default async function handler(req, res) {
 
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'gallery.json');
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME
+const apiKey = process.env.CLOUDINARY_API_KEY
+const apiSecret = process.env.CLOUDINARY_API_SECRET
 
-  try {
-    const fileData = fs.readFileSync(filePath, 'utf8');
-    const data = JSON.parse(fileData);
+const auth = Buffer.from(`${apiKey}:${apiSecret}`).toString("base64")
 
-    res.status(200).json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to load gallery" });
-  }
+try{
+
+const response = await fetch(
+`https://api.cloudinary.com/v1_1/${cloudName}/resources/image`,
+{
+headers:{
+Authorization:`Basic ${auth}`
+}
+}
+)
+
+const data = await response.json()
+
+const images = data.resources.map(img => img.secure_url)
+
+res.status(200).json({images})
+
+}catch(err){
+
+res.status(500).json({error:"Failed to load gallery"})
+
+}
+
 }
