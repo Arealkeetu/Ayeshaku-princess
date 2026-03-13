a335b3d95a8f9350406568c2aa35898ce7c9cfc1
Arealkeetu <airilsyazani25@gmail.com>
2026-03-13 15:21:32 +0800

    Update loadGallery.js

--- api/loadGallery.js
+++ api/loadGallery.js
@@ -1,26 +1,16 @@
-export default async function handler(req, res) {
+import fs from 'fs';
+import path from 'path';
 
-  const cloudName = "dxyl0aoi0";
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'gallery.json');
 
   try {
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(fileData);
 
-    const response = await fetch(
-      `https://res.cloudinary.com/${cloudName}/image/list/my-gallery.json`
-    );
-
-    const data = await response.json();
-
-    const images = data.resources.map(img =>
-      `https://res.cloudinary.com/${cloudName}/image/upload/${img.public_id}.${img.format}`
-    );
-
-    res.status(200).json({ images });
-
+    res.status(200).json(data);
   } catch (err) {
-
     console.error(err);
     res.status(500).json({ error: "Failed to load gallery" });
-
   }
-
 }
